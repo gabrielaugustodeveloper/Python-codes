b2bdbfa624229aa8e2c5b0d420927aab715e5046
gabrielaugustodeveloper <augusto.dev.g@gmail.com>
2024-08-27 17:52:52 -0300

    .

--- Bootcamp/Day02/billcalculator.py
+++ Bootcamp/Day02/billcalculator.py
@@ -2,3 +2,10 @@ print("Welcome to the tip calculator!")
 bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10 12 15 "))
 people = int(input("How many people to split the bill? "))
+tip_str = str(tip)
+totaltip = "1." + tip_str
+#print(totaltip)
+
+
+total = bill/people * float(totaltip)
+print(f"The bill per person is ${total}")
