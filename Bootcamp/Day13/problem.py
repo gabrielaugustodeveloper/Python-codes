1cb8382396cb861146d71dfe2662821df5cbb62a
gabrielaugustodeveloper <augusto.dev.g@gmail.com>
2026-02-17 12:13:32 -0300

    Create problem.py

---
+++ Bootcamp/Day13/problem.py
@@ -0,0 +1,19 @@
+
+#   wrong
+#def my_function():
+#    for i in range(1, 20):
+#        if i == 20:
+#            print("You got it")
+
+
+#my_function()
+
+#right
+
+def my_function():
+    for i in range(1, 21):
+        if i == 20:
+            print("You got it")
+
+
+my_function()
