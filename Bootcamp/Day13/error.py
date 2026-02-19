84270749467aa7f27d2f97a2cb08e36ac3e02596
gabrielaugustodeveloper <augusto.dev.g@gmail.com>
2026-02-19 11:10:52 -0300

    .

---
+++ Bootcamp/Day13/error.py
@@ -0,0 +1,14 @@
+#WRONG
+#age = int(input("How old are you?"))
+#if age > 18:
+#print("You can drive at age {age}.")
+
+#RIGHT
+try:
+    age = int(input("How old are you?"))
+except ValueError:
+    print("You have typed in a an invalid number. Please try again with a numerical response such as 15.")
+    age = int(input("How old are you?"))
+
+if age > 18:
+    print(f"You can drive at age {age}.")
---
+++ Bootcamp/Day13/year.py
@@ -0,0 +1,16 @@
+#WRONG
+#year = int(input("What's your year of birth?"))
+
+#if year > 1980 and year < 1994:
+#    print("You are a millennial.")
+#elif year > 1994:
+#    print("You are a Gen Z.")
+
+
+#RIGHT
+year = int(input("What's your year of birth?"))
+
+if year > 1980 and year < 1994:
+    print("You are a millennial.")
+elif year >= 1994:
+    print("You are a Gen Z.")
